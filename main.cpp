df3470d203b45bcb6052f32a212e8c8d4ff50fd5
melizo4ka <elizaveta.mochalova@stud.unifi.it>
2024-02-27 15:32:00 +0100

    other parallelization methods

--- main.cpp
+++ main.cpp
@@ -16,14 +16,14 @@ CircleData createCircle(int x, int y, int z, int opacity) {
     data.circle.setPosition(static_cast<float>(x), static_cast<float>(y));
 
     //random generate for colours
-    std::random_device rd;
-    std::mt19937 gen(rd());
+    std::random_device rdc;
+    std::mt19937 genCol(rdc());
     std::uniform_int_distribution<int> colorDist(0, 255);
 
     int colour[3];
-    colour[0] = colorDist(gen);
-    colour[1] = colorDist(gen);
-    colour[2] = colorDist(gen);
+    colour[0] = colorDist(genCol);
+    colour[1] = colorDist(genCol);
+    colour[2] = colorDist(genCol);
     data.circle.setFillColor(sf::Color(colour[0], colour[1], colour[2], opacity));
 
     data.depth = z;
@@ -34,7 +34,6 @@ CircleData createCircle(int x, int y, int z, int opacity) {
 int main() {
     // Initialize Xlib for multi-threading
     XInitThreads();
-
     //checking of num of processors
 #ifdef _OPENMP
     std::cout << "_OPENMP defined" << std::endl;
@@ -42,21 +41,25 @@ int main() {
 #endif
 
     //more than a 1000 can crash on my PC
-    int numberOfShapes = 1100;
+    int numberOfShapes = 10000;
 
     CircleData shapes[numberOfShapes];
-
     //variable to get time til image is displayed
     bool displayTime = true;
     sf::Clock clock;
 
+    //random generate for colours
+    std::random_device rdp;
+    std::mt19937 genPos(rdp());
+    std::uniform_int_distribution<int> posXDist(0, sf::VideoMode::getDesktopMode().width - 150);
+    std::uniform_int_distribution<int> posYDist(0, sf::VideoMode::getDesktopMode().height - 150);
+    std::uniform_int_distribution<int> posZDist(0, 50);
+
 #pragma omp parallel for
     for (int i = 0; i < numberOfShapes; i++) {
-        //added condition of "-150" to have all circles drawn inside the window
-        int xPosition = rand() % (sf::VideoMode::getDesktopMode().width - 150);
-        int yPosition = rand() % (sf::VideoMode::getDesktopMode().height - 150);
-
-        int zPosition = rand() % 100;
+        int xPosition = posXDist(genPos);
+        int yPosition = posYDist(genPos);
+        int zPosition = posZDist(genPos);
         int opacity = 50;
 
         //creating shaped one at a time and storing into array
@@ -64,15 +67,10 @@ int main() {
     }
 
     // Sort the array based on depth, to draw from back to front
-#pragma omp parallel
-    {
-#pragma omp single
-        {
-            std::sort(shapes, shapes + numberOfShapes, [](const CircleData &a, const CircleData &b) {
-                return a.depth > b.depth;
-            });
-        }
-    }
+    std::sort(shapes, shapes + numberOfShapes, [](const CircleData& a, const CircleData& b) {
+        return a.depth > b.depth;
+    });
+
 
     sf::RenderWindow window(sf::VideoMode(sf::VideoMode::getDesktopMode().width,
                                           sf::VideoMode::getDesktopMode().height), "Renderer");
@@ -80,7 +78,7 @@ int main() {
     while (window.isOpen()) {
         sf::Event event;
         while (window.pollEvent(event)) {
-            if (event.type == sf::Event::Closed) {
+            if (event.type == sf::Event::Closed){
                 //to save window image produced, file is saved to bin folder
                 sf::Texture texture;
                 texture.create(window.getSize().x, window.getSize().y);
@@ -95,12 +93,14 @@ int main() {
         //white background for aesthetic purposes
         window.clear(sf::Color::White);
 
-        /*
+#pragma omp for
         //drawing of circles from ordered array
         for (int i = 0; i < numberOfShapes; i++) {
             window.draw(shapes[i].circle);
         }
-*/
+
+        window.display();
+
         //stop counting time here
         if (displayTime) {
             sf::Time elapsed = clock.getElapsedTime();
@@ -109,5 +109,6 @@ int main() {
         }
 
     }
+
     return 0;
 }
