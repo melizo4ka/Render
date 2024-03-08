1c45af51f248768f17afd2b9384994c74f90bdea
melizo4ka <elizaveta.mochalova@stud.unifi.it>
2024-03-08 14:37:19 +0100

    code now executes first in parallel, then in sequence

--- main.cpp
+++ main.cpp
@@ -2,8 +2,8 @@
 #include <SFML/Graphics.hpp>
 #include <iostream>
 #include <random>
-#include <omp.h>
-#include <X11/Xlib.h>
+#include <sstream>
+
 
 struct CircleData {
     sf::CircleShape circle;
@@ -14,22 +14,21 @@ CircleData createCircle(int x, int y, int z, int opacity) {
     CircleData data;
 
     //random radius for circles
-    std::random_device rdr;
-    std::mt19937 genRad(rdr());
+    std::random_device rd;
+    std::mt19937 genRad(rd());
     std::uniform_int_distribution<int> radiusDist(5, 50);
     int radius = radiusDist(genRad);
     data.circle.setRadius(static_cast<float>(radius));
 
     data.circle.setPosition(static_cast<float>(x), static_cast<float>(y));
     //random generate for colours
-    std::random_device rdc;
-    std::mt19937 genCol(rdc());
+    std::mt19937 genCol(rd());
     std::uniform_int_distribution<int> colorDist(0, 255);
-
     int colour[3];
     colour[0] = colorDist(genCol);
     colour[1] = colorDist(genCol);
     colour[2] = colorDist(genCol);
+
     data.circle.setFillColor(sf::Color(colour[0], colour[1], colour[2], opacity));
     data.depth = z;
 
@@ -37,35 +36,29 @@ CircleData createCircle(int x, int y, int z, int opacity) {
 }
 
 int main() {
-    // Initialize Xlib for multi-threading
-    XInitThreads();
-    //checking of num of processors
-#ifdef _OPENMP
-    std::cout << "_OPENMP defined" << std::endl;
-    std::cout << "Num processors (Phys+HT): " << omp_get_num_procs() << std::endl;
-#endif
+    std::vector<float> parTimes;
+    std::vector<float> seqTimes;
+    std::vector<float> speedup;
+    // first value should be discarded
+    int differentNoS[] = {0, 10, 100, 500, 1000, 10000, 20000};
 
-    // Set a flag to decide whether to execute the parallel loop or not
+    // parallel section
     bool executeParallelLoop = true;
-    if (executeParallelLoop)
-        std::cout << "This is parallel execution." << std::endl;
-    else
-        std::cout << "This is sequential execution." << std::endl;
-    int differentNoS[] = {10, 100, 500, 1000, 10000, 20000};
+    std::cout << "Start of parallel execution." << std::endl;
 
     for (int numberOfShapes : differentNoS){
         CircleData shapes[numberOfShapes];
 
-        //variable to get time til image is displayed
-        bool displayTime = true;
-        sf::Clock clock;
-
-        //random generate for colours
+        //random generate for position
         std::random_device rdp;
         std::mt19937 genPos(rdp());
         std::uniform_int_distribution<int> posXDist(0, sf::VideoMode::getDesktopMode().width - 150);
         std::uniform_int_distribution<int> posYDist(0, sf::VideoMode::getDesktopMode().height - 150);
         std::uniform_int_distribution<int> posZDist(0, 50);
+
+        //variable to get time til image is displayed
+        bool displayTime = true;
+        sf::Clock clock;
 
 #pragma omp parallel for if(executeParallelLoop)
         for (int i = 0; i < numberOfShapes; i++) {
@@ -85,18 +78,22 @@ int main() {
 
 
         sf::RenderWindow window(sf::VideoMode(sf::VideoMode::getDesktopMode().width,
-                                              sf::VideoMode::getDesktopMode().height), "Renderer");
+                                              sf::VideoMode::getDesktopMode().height), "Renderer Parallel");
 
         while (window.isOpen()) {
             sf::Event event;
             while (window.pollEvent(event)) {
                 if (event.type == sf::Event::Closed){
+                    std::ostringstream filenameStream;
+                    filenameStream << "screenshotPar_" << numberOfShapes << "Shapes.jpg";
+                    std::string filename = filenameStream.str();
+
                     //to save window image produced, file is saved to bin folder
                     sf::Texture texture;
                     texture.create(window.getSize().x, window.getSize().y);
                     texture.update(window);
                     sf::Image screenshot = texture.copyToImage();
-                    screenshot.saveToFile("screenshot.jpg");
+                    screenshot.saveToFile(filename);
                     window.close();
                 }
             }
@@ -104,32 +101,97 @@ int main() {
             //white background for aesthetic purposes
             window.clear(sf::Color::White);
 
-            if (executeParallelLoop) {
 #pragma omp for
-                //drawing of circles from ordered array
-                for (int i = 0; i < numberOfShapes; i++) {
-                    window.draw(shapes[i].circle);
+            for (int i = 0; i < numberOfShapes; i++) {
+                window.draw(shapes[i].circle);
+            }
+            window.display();
+
+            if (displayTime) {
+                sf::Time elapsed = clock.getElapsedTime();
+                // std::cout << "Elapsed time: " << elapsed.asSeconds() << " seconds for " << numberOfShapes << " shapes." << std::endl;
+                parTimes.push_back(elapsed.asSeconds());
+                displayTime = false;
+            }
+        }
+    }
+
+    std::cout << "Start of sequential execution." << std::endl;
+    for (int numberOfShapes : differentNoS){
+        CircleData shapes[numberOfShapes];
+
+        std::random_device rdp;
+        std::mt19937 genPos(rdp());
+        std::uniform_int_distribution<int> posXDist(0, sf::VideoMode::getDesktopMode().width - 150);
+        std::uniform_int_distribution<int> posYDist(0, sf::VideoMode::getDesktopMode().height - 150);
+        std::uniform_int_distribution<int> posZDist(0, 50);
+
+        bool displayTime = true;
+        sf::Clock clock;
+
+        for (int i = 0; i < numberOfShapes; i++) {
+            int xPosition = posXDist(genPos);
+            int yPosition = posYDist(genPos);
+            int zPosition = posZDist(genPos);
+            int opacity = 50;
+
+            shapes[i] = createCircle(xPosition, yPosition, zPosition, opacity);
+        }
+
+        std::sort(shapes, shapes + numberOfShapes, [](const CircleData& a, const CircleData& b) {
+            return a.depth > b.depth;
+        });
+
+
+        sf::RenderWindow window(sf::VideoMode(sf::VideoMode::getDesktopMode().width,
+                                              sf::VideoMode::getDesktopMode().height),
+                                "Renderer Sequential");
+
+        while (window.isOpen()) {
+            sf::Event event;
+            while (window.pollEvent(event)) {
+                if (event.type == sf::Event::Closed){
+                    std::ostringstream filenameStream;
+                    filenameStream << "screenshotSeq_" << numberOfShapes << "Shapes.jpg";
+                    std::string filename = filenameStream.str();
+                    sf::Texture texture;
+                    texture.create(window.getSize().x, window.getSize().y);
+                    texture.update(window);
+                    sf::Image screenshot = texture.copyToImage();
+                    screenshot.saveToFile(filename);
+                    window.close();
                 }
             }
-            else{
-                //drawing of circles from ordered array
-                for (int i = 0; i < numberOfShapes; i++) {
-                    window.draw(shapes[i].circle);
-                }
+            window.clear(sf::Color::White);
+
+            for (int i = 0; i < numberOfShapes; i++) {
+                window.draw(shapes[i].circle);
             }
 
             window.display();
 
-            //stop counting time here
             if (displayTime) {
                 sf::Time elapsed = clock.getElapsedTime();
-                std::cout << "Elapsed time: " << elapsed.asSeconds() << " seconds for " << numberOfShapes << " shapes." << std::endl;
+                // std::cout << "Elapsed time: " << elapsed.asSeconds() << " seconds for " << numberOfShapes << " shapes." << std::endl;
+                seqTimes.push_back(elapsed.asSeconds());
                 displayTime = false;
             }
 
         }
 
     }
+    std::cout << std::endl;
+
+    for (size_t i = 0; i < seqTimes.size(); ++i) {
+        double result = seqTimes[i] / parTimes[i];
+        speedup.push_back(result);
+    }
+    // Print the result vector
+    std::cout << "Result vector for speedup: ";
+    for (const auto &element : speedup) {
+        std::cout << element << " ";
+    }
+    std::cout << std::endl;
 
     return 0;
 }
