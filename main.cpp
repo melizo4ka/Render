5a0e617b6bcb231a8b82513213762ba9394e2012
melizo4ka <64137900+melizo4ka@users.noreply.github.com>
2024-12-02 10:48:30 +0100

    changes to blending functions

--- main.cpp
+++ main.cpp
@@ -31,7 +31,7 @@ struct PixelData {
         pixelColour.r = 0;
         pixelColour.g = 0;
         pixelColour.b = 0;
-        pixelColour.a = 255;
+        pixelColour.a = 0;
     }
 };
 
@@ -92,7 +92,7 @@ sf::Color blendColors(const sf::Color& color2, const sf::Color& color1) {
     resultColor.r = static_cast<sf::Uint8>((color1.r + color2.r) / 2);
     resultColor.g = static_cast<sf::Uint8>((color1.g + color2.g) / 2);
     resultColor.b = static_cast<sf::Uint8>((color1.b + color2.b) / 2);
-    resultColor.a = static_cast<sf::Uint8>(((alpha1+ alpha2) / 2) * 255.0f);
+    //resultColor.a = static_cast<sf::Uint8>(((alpha1+ alpha2) / 2) * 255.0f);
 
     return resultColor;
 }
@@ -101,30 +101,13 @@ sf::Color blendColors(const sf::Color& color2, const sf::Color& color1) {
 sf::Color blendColorsBitmaps(const sf::Color& color2, const sf::Color& color1) {
     float alpha1 = static_cast<float>(color1.a) / 255.0f;
     float alpha2 = static_cast<float>(color2.a) / 255.0f;
-    float resultAlpha = alpha1 + alpha2 * (1 - alpha1);
 
     sf::Color resultColor;
     resultColor.r = static_cast<sf::Uint8>((color1.r * alpha1) + (color2.r * alpha2 * (1 - alpha1)));
     resultColor.g = static_cast<sf::Uint8>((color1.g * alpha1) + (color2.g * alpha2 * (1 - alpha1)));
     resultColor.b = static_cast<sf::Uint8>((color1.b * alpha1) + (color2.b * alpha2 * (1 - alpha1)));
-    resultColor.a = static_cast<sf::Uint8>(resultAlpha * 255.0f);
 
     return resultColor;
-}
-
-sf::Color calculateRGBA (const std::vector<circleInfluence>& influences){
-    sf::Color result;
-    sf::Color inter_result;
-    result = blendColors( influences[0].infColour, influences[1].infColour);
-    if (influences.size() == 2)
-        return result;
-    else{
-        for (int k = 2; k < influences.size(); k++){
-            inter_result = blendColors( result,influences[k].infColour);
-            result = inter_result;
-        }
-        return result;
-    }
 }
 
 sf::Image bitmapToImage(BitMap& bitmap) {
@@ -163,31 +146,55 @@ int main() {
     std::uniform_int_distribution<int> posYDist(0, imgHeight - 2 * offsetCenter);
     std::uniform_int_distribution<int> posZDist(0, maxDepth);
 
-    std::vector<int> differentNoS = {2000};
+    int numberOfShapes = 1000;
+    std::map<int, std::vector<CircleData>> depthMap;
 
+    bool displayTime = true;
 
-    for (int numberOfShapes: differentNoS) {
-        bool displayTime = true;
-        CircleData shapes[numberOfShapes];
+    for (int i = 0; i < numberOfShapes; i++) {
+        int xPosition = posXDist(gen);
+        int yPosition = posYDist(gen);
+        int zPosition = posZDist(gen);
+        CircleData circleData = createCircle(xPosition, yPosition, zPosition, gen);
+        depthMap[zPosition].push_back(circleData);
+    }
 
-        // creation of cicles, not accounting that into the program's time
-        for (int i = 0; i < numberOfShapes; i++) {
-            int xPosition = posXDist(gen);
-            int yPosition = posYDist(gen);
-            int zPosition = posZDist(gen);
-            shapes[i] = createCircle(xPosition, yPosition, zPosition, gen);
+    std::cout << "Start of sequential execution." << std::endl;
+    sf::Clock clockSeq;
+
+    sf::RenderWindow windowSeq(sf::VideoMode(imgWidth,
+                                             imgHeight), "Renderer Sequential");
+    for (auto it = depthMap.rbegin(); it != depthMap.rend(); ++it) {
+        // sorting the circles by depth
+        std::sort(it->second.begin(), it->second.end(),
+                  [](const CircleData& a, const CircleData& b) {
+                      return a.depth > b.depth;
+                  });
+
+        for (const CircleData& circle : it->second) {
+            windowSeq.draw(circle.circle);
         }
+    }
 
-        std::cout << "Start of sequential execution." << std::endl;
-        sf::Clock clockSeq;
+    sf::Texture texture;
+    texture.create(imgWidth, imgHeight);
+    texture.update(windowSeq);
+    sf::Image screenshot = texture.copyToImage();
+    screenshot.saveToFile("screenSeq_" + std::to_string(numberOfShapes) + "Shapes.jpg");
 
-        // Sort the array based on depth, to draw from back to front
-        std::sort(shapes, shapes + numberOfShapes, [](const CircleData &a, const CircleData &b) {
-            return a.depth > b.depth;
-        });
 
-        sf::RenderWindow windowSeq(sf::VideoMode(imgWidth,
-                                                 imgHeight), "Renderer Sequential");
+    while (windowSeq.isOpen()) {
+        sf::Event event;
+        while (windowSeq.pollEvent(event)) {
+            if (event.type == sf::Event::Closed) {
+                windowSeq.close();
+            }
+        }
+        windowSeq.clear();
+
+        sf::Sprite sprite(texture);
+        windowSeq.draw(sprite);
+        windowSeq.display();
 
         if (displayTime) {
             sf::Time elapsed = clockSeq.getElapsedTime();
@@ -196,133 +203,9 @@ int main() {
             seqTimes.push_back(elapsed.asSeconds());
             displayTime = false;
         }
+    }
 
-        while (windowSeq.isOpen()) {
-            sf::Event event;
-            while (windowSeq.pollEvent(event)) {
-                if (event.type == sf::Event::Closed) {
-                    std::ostringstream filenameStream;
-                    filenameStream << "screenSeq_" << numberOfShapes << "Shapes.jpg";
-                    std::string filename = filenameStream.str();
-                    sf::Texture texture;
-                    texture.create(windowSeq.getSize().x, windowSeq.getSize().y);
-                    texture.update(windowSeq);
-                    sf::Image screenshot = texture.copyToImage();
-                    screenshot.saveToFile(filename);
-                    windowSeq.close();
-                }
-            }
-            windowSeq.clear();
-
-            for (int i = 0; i < numberOfShapes; i++) {
-                //if(shapes[i].depth == 0)
-                windowSeq.draw(shapes[i].circle);
-            }
-            windowSeq.display();
-
-
-        }
-/*
-        std::cout << "Start of parallel execution (First Version)." << std::endl;
-        displayTime = true;
-        sf::Clock clockPar;
-
-        //iterations to create the bitmap pixel by pixel
-        BitMap bitmap(imgWidth, imgHeight);
-
-        omp_set_num_threads(2);
-#pragma omp parallel default(none) shared(bitmap, shapes, numberOfShapes, imgWidth, imgHeight, cout)
-        {
-            //#pragma omp single
-            //std::cout << "Number of threads: " << omp_get_num_threads() << std::endl;
-
-#pragma omp for collapse(2) schedule(dynamic, 1) nowait
-            for (int j = 0; j < imgHeight; j++) {
-                for (int i = 0; i < imgWidth; i++) {
-                    //creating new data structure
-                    std::vector<circleInfluence> influences;
-                    // iterate over all the circles
-                    for (int n = 0; n < numberOfShapes; n++) {
-                        // call checker
-                        if (pixelInCircle(shapes[n].circle, i, j)) {
-                            // if the condition is true then we add info of that circle in the data structure
-                            influences.emplace_back(shapes[n].depth, shapes[n].circle.getFillColor());
-                        }
-                    }
-
-                    // this part of the code is not used for now since shapes is already ordered in the sequential section
-                    if (!influences.empty()) {
-                        if (influences.size() > 1) {
-                            std::sort(influences.begin(), influences.end(),
-                                      [](const circleInfluence &a, const circleInfluence &b) {
-                                          return a.depth > b.depth;
-                                      });
-
-                        }
-                    }
-                    sf::Color finalColor;
-                    if (influences.size() > 1) {
-                        finalColor = calculateRGBA(influences);
-                        //bitmap.getPixel(i, j).pixelColour = finalColor;
-                    } else {
-                        // just put values into bitmap
-                        if (!influences.empty())
-                            finalColor = influences[0].infColour;
-                        //bitmap.getPixel(i, j).pixelColour = influences[0].infColour;
-                    }
-#pragma omp atomic write
-                    bitmap.getPixel(i, j).pixelColour.r = finalColor.r;
-
-#pragma omp atomic write
-                    bitmap.getPixel(i, j).pixelColour.g = finalColor.g;
-
-#pragma omp atomic write
-                    bitmap.getPixel(i, j).pixelColour.b = finalColor.b;
-
-#pragma omp atomic write
-                    bitmap.getPixel(i, j).pixelColour.a = finalColor.a;
-                }
-            }
-        }
-        sf::Image image = bitmapToImage(bitmap);
-        sf::RenderWindow windowPar(sf::VideoMode(imgWidth,
-                                                 imgHeight), "Renderer Parallel");
-
-        if (displayTime) {
-            sf::Time elapsed = clockPar.getElapsedTime();
-            std::cout << "Elapsed time: " << elapsed.asSeconds() << " seconds for " << numberOfShapes << " shapes."
-                      << std::endl;
-            parTimes.push_back(elapsed.asSeconds());
-            displayTime = false;
-        }
-
-        while (windowPar.isOpen()) {
-            sf::Event event;
-            while (windowPar.pollEvent(event)) {
-                if (event.type == sf::Event::Closed) {
-
-                    std::ostringstream filenameStream;
-                    filenameStream << "screenPar_" << numberOfShapes << "Shapes.jpg";
-                    std::string filename = filenameStream.str();
-                    sf::Texture texture;
-                    texture.create(windowPar.getSize().x, windowPar.getSize().y);
-                    texture.update(windowPar);
-                    sf::Image screenshot = texture.copyToImage();
-                    screenshot.saveToFile(filename);
-                    windowPar.close();
-                }
-            }
-            windowPar.clear();
-            sf::Texture texture;
-            texture.loadFromImage(image);
-            sf::Sprite sprite(texture);
-            windowPar.draw(sprite);
-            windowPar.display();
-
-
-        }
-
-*/
+        /*
         std::cout << "Start of parallel execution (Second version)." << std::endl;
         displayTime = true;
         sf::Clock clockPar;
@@ -352,12 +235,11 @@ int main() {
                         BitMap& bitmapTmp = bitmapsWithDepth[shapes[n].depth];
                         sf::Color colorTmp = bitmapTmp.getPixel(i, j).pixelColour;
                         // check if there is already a colour written in that pixel
-                        //TODO add atomic for writing
                         if((int)colorTmp.r == 0 && (int)colorTmp.g == 0 && (int)colorTmp.b == 0 && (int)colorTmp.a == 255)
                             bitmapTmp.getPixel(i, j).pixelColour = shapes[n].circle.getFillColor();
                         else
                             // do the blending
-                            bitmapTmp.getPixel(i, j).pixelColour = blendColors(colorTmp, shapes[n].circle.getFillColor());
+                            bitmapTmp.getPixel(i, j).pixelColour = blendColorsBitmaps(colorTmp, shapes[n].circle.getFillColor());
                     }
                 }
             }
@@ -367,8 +249,8 @@ int main() {
         for (int j = 0; j < imgHeight; ++j) {
             for (int i = 0; i < imgWidth; ++i) {
                 // Iterate through all bitmaps (corresponding to different depths)
-                //for (int k = 0; k >= 0; k--){
-                for (int k = numBitmaps; k >= 0; k--) {
+                for (int k = 0; k >= 0; k--){
+                //for (int k = numBitmaps; k >= 0; k--) {
                     sf::Color colorTmp = bitmapsWithDepth[k].getPixel(i, j).pixelColour;
                     if ((int)colorTmp.r != 0 || (int)colorTmp.g != 0 || (int)colorTmp.b != 0 || (int)colorTmp.a != 255) {
                         // blend colors
@@ -418,6 +300,110 @@ int main() {
 
 
     }
+         */
+    std::cout << "Start of parallel execution with OpenMP. Third Version." << std::endl;
+    displayTime = true;
+    sf::Clock clockPar;
+    std::map<int, BitMap> bitmapsWithDepth;
+    const int numBitmaps = maxDepth + 1;
+
+    for (int i = 0; i <= maxDepth; ++i) {
+        bitmapsWithDepth.emplace(i, BitMap(imgWidth, imgHeight));
+    }
+
+    BitMap bitmapFinal(imgWidth, imgHeight);
+
+#pragma omp parallel default(none) shared(depthMap, bitmapsWithDepth, imgWidth, imgHeight, maxDepth)
+{
+#pragma omp for
+    for (int depth = 0; depth <= maxDepth; ++depth) {
+        auto it = depthMap.find(depth);
+        if (it != depthMap.end()) {
+            const std::vector<CircleData>& circles = it->second;
+            BitMap& bitmapTmp = bitmapsWithDepth[depth];
+
+            for (const CircleData& circle : circles) {
+                float radius = circle.circle.getRadius();
+                float centerX = circle.circle.getPosition().x + radius;
+                float centerY = circle.circle.getPosition().y + radius;
+
+                for (int j = static_cast<int>(centerY - radius); j <= static_cast<int>(centerY + radius); ++j) {
+                    for (int i = static_cast<int>(centerX - radius); i <= static_cast<int>(centerX + radius); ++i) {
+                        // Check if the pixel is inside the circle and within the window bounds
+                        if (pixelInCircle(circle.circle, i, j) && i >= 0 && i < imgWidth && j >= 0 && j < imgHeight) {
+                            // get current bitmap's pixel colour
+                            sf::Color colorTmp = bitmapTmp.getPixel(i, j).pixelColour;
+
+                            // Only write if no previous color exists, otherwise blend colors
+                            if ((int)colorTmp.r == 0 && (int)colorTmp.g == 0 &&
+                                (int)colorTmp.b == 0 && (int)colorTmp.a == 0) {
+                                bitmapTmp.getPixel(i, j).pixelColour = circle.circle.getFillColor();
+                            } else {
+                                bitmapTmp.getPixel(i, j).pixelColour =
+                                        blendColorsBitmaps(colorTmp, circle.circle.getFillColor());
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+    //bitmapFinal = bitmapsWithDepth[9];
+    for (int j = 0; j < imgHeight; ++j) {
+        for (int i = 0; i < imgWidth; ++i) {
+            for (auto it = bitmapsWithDepth.rbegin(); it != bitmapsWithDepth.rend(); ++it) {
+                sf::Color colorTmp = it->second.getPixel(i, j).pixelColour;
+                sf::Color colorFinalTmp = bitmapFinal.getPixel(i, j).pixelColour;
+                // base layer blank
+                if ((int)colorFinalTmp.r == 0 && (int)colorFinalTmp.g == 0 &&
+                    (int)colorFinalTmp.b == 0 && (int)colorFinalTmp.a == 0){
+                    bitmapFinal.getPixel(i, j).pixelColour = colorTmp;
+                }
+                // pixel has colour
+                else if (colorTmp.a > 0) {
+                    bitmapFinal.getPixel(i, j).pixelColour =
+                            blendColorsBitmaps(bitmapFinal.getPixel(i, j).pixelColour, colorTmp);
+                }
+            }
+        }
+    }
+    sf::Image image = bitmapToImage(bitmapFinal);
+    sf::RenderWindow windowPar(sf::VideoMode(imgWidth, imgHeight), "Renderer Parallel");
+
+    while (windowPar.isOpen()) {
+        sf::Event event;
+        while (windowPar.pollEvent(event)) {
+            if (event.type == sf::Event::Closed) {
+                // Save the final rendered image to a file
+                std::ostringstream filenameStream;
+                filenameStream << "screenPar_" << numberOfShapes << "Shapes.jpg";
+                std::string filename = filenameStream.str();
+                sf::Texture texture;
+                texture.create(windowPar.getSize().x, windowPar.getSize().y);
+                texture.update(windowPar);
+                sf::Image screenshot = texture.copyToImage();
+                screenshot.saveToFile(filename);
+                windowPar.close();
+            }
+        }
+
+        windowPar.clear();
+        sf::Texture texture;
+        texture.loadFromImage(image);
+        sf::Sprite sprite(texture);
+        windowPar.draw(sprite);
+        windowPar.display();
+        if (displayTime) {
+            sf::Time elapsed = clockPar.getElapsedTime();
+            std::cout << "Elapsed time: " << elapsed.asSeconds() << " seconds for " << numberOfShapes << " shapes."
+                      << std::endl;
+            parTimes.push_back(elapsed.asSeconds());
+            displayTime = false;
+        }
+    }
+
+
 
 //SPEED UP CALCULATOR
 /*
